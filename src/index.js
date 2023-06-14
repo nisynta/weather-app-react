ec837cd83ab68b18a1d25371267147ed28b8b49a
Nisya Yunita <nisya.yunita@inmotion.co.id>
2023-06-14 22:14:21 +0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.css";
+import WeatherData from './pages/WeatherData';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <WeatherData />
   </React.StrictMode>
 );
 
